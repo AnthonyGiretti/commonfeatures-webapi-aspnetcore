52f5bc5ca4e2f652ddd89ed4bee13104da9c8852
Anthony Giretti <anthony.giretti@gmail.com>
2018-09-15 11:34:15 -0400

    add authentication by token example

--- WebApiDemo/Controllers/ValuesController.cs
+++ WebApiDemo/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiDemo.Controllers
@@ -11,6 +12,7 @@ namespace WebApiDemo.Controllers
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        [Authorize]
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -18,6 +20,7 @@ namespace WebApiDemo.Controllers
         }
 
         // GET api/values/5
+        [Authorize]
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,21 +28,18 @@ namespace WebApiDemo.Controllers
         }
 
         // POST api/values
+        [Authorize(Policy = "SurveyCreator")]
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
+        // POST api/values
+        [Authorize(Policy = "SuperSurveyCreator")]
+        [HttpDelete]
         public void Delete(int id)
         {
         }
+
     }
 }
--- WebApiDemo/Startup.cs
+++ WebApiDemo/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace WebApiDemo
 {
@@ -24,6 +25,34 @@ namespace WebApiDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://login.microsoftonline.com/136544d9-038e-4646-afff-10accb370679";
+                options.Audience = "257b6c36-1168-4aac-be93-6f2cd81cec43";
+                options.TokenValidationParameters.ValidateLifetime = true;
+                options.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
+            });
+
+            services.AddAuthorization(opts =>
+            {
+                opts.AddPolicy("SurveyCreator", p =>
+                {
+                    p.RequireClaim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "SurveyCreator");
+
+                });
+
+                opts.AddPolicy("SuperSurveyCreator", p =>
+                {
+                    p.RequireClaim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "SurveyCreator");
+                    p.RequireClaim("groups", "8115e3be-ac7a-4886-a1e6-5b6aaf810a8f");
+
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,6 +63,8 @@ namespace WebApiDemo
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }
