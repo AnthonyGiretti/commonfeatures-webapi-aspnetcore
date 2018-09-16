c5a7ccbd0d1cc79be9fe3e72e89c791703d09447
Anthony Giretti <anthony.giretti@gmail.com>
2018-09-16 18:19:46 -0400

    add more xample for profiling

--- WebApiDemo/Controllers/DemoProfilingController.cs
+++ WebApiDemo/Controllers/DemoProfilingController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,12 +18,35 @@ namespace WebApiDemo.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            var profiler = MiniProfiler.StartNew("Get method");
-            using (profiler.Step("Get Work"))
+            string url1 = string.Empty;
+            string url2 = string.Empty;
+
+            using (MiniProfiler.Current.Step("Get method"))
             {
-                return new string[] { "value1", "value2" };
+                using (MiniProfiler.Current.Step("Prepare data"))
+                {
+                    // Could be a SQL call
+                    Thread.Sleep(500);
+                    url1 = "https://google.com";
+                    url2 = "https://stackoverflow.com/";
+                }
+                using (MiniProfiler.Current.Step("Use data for http call"))
+                {
+                    using (MiniProfiler.Current.CustomTiming("http", "GET " + url1))
+                    {
+                        var client = new WebClient();
+                        var reply = client.DownloadString(url1);
+                    }
+
+                    using (MiniProfiler.Current.CustomTiming("http", "GET " + url2))
+                    {
+                        var client = new WebClient();
+                        var reply = client.DownloadString(url2);
+                    }
+                }
+                
             }
-            
+            return new string[] { "value1", "value2" };        
         }
 
     }
