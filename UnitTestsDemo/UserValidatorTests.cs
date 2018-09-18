1237136923ae37c4b8d7c5a1ca829a4add961b62
Anthony Giretti <anthony.giretti@gmail.com>
2018-09-18 19:51:29 -0400

    add unit tests to fluent validation validators

---
+++ UnitTestsDemo/UserValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using WebApiDemo.Validators;
+using Xunit;
+using System.Linq;
+
+namespace UnitTestsDemo
+{
+    public class UserValidatorTests
+    {
+        [Fact]
+        public void WhenAUserIsWellSet_ValidationShouldSucceeed()
+        {
+            // Arrange
+            var validator = new UserValidator();
+            var user = new WebApiDemo.Models.User
+            {
+                Gender = "test",
+                SIN = "510390115",
+                FirstName = "anthony",
+                LastName = "giretti"
+            };
+
+            // Act
+            ValidationResult validationResult = validator.Validate(user);
+
+            // Assert
+            validationResult
+            .IsValid
+            .Should()
+            .BeTrue();
+        }
+
+        [Fact]
+        public void WhenASINIsNotValid_ValidationShouldFail()
+        {
+            // Arrange
+            var validator = new UserValidator();
+            var user = new WebApiDemo.Models.User
+            {
+                Gender = "test",
+                SIN = "123456789",
+                FirstName = "anthony",
+                LastName = "giretti"
+            };
+
+            // Act
+            ValidationResult validationResult = validator.Validate(user);
+
+            // Assert
+            validationResult
+            .IsValid
+            .Should()
+            .BeFalse();
+
+            validationResult
+            .Errors
+            .Select(x=> x.ErrorMessage)
+            .Should()
+            .Contain("SIN (123456789) is not valid.");
+        }
+    }
+}
