096693c7f5b287191b5ccefb1dd7853da38efb54
Anthony Giretti <anthony.giretti@gmail.com>
2018-12-02 21:08:39 -0500

    Create an IAuthorizationRequirement

---
+++ WebApiDemo/AuthorizationHandlers/SuperSurveyCreatorRequirement.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.AuthorizationHandlers
+{
+    public class SuperSurveyCreatorRequirement : IAuthorizationRequirement
+    {
+        public SuperSurveyCreatorRequirement(string role, string group)
+        {
+            Group = group;
+            Role = role;
+        }
+
+        public string Role { get; private set; }
+        public string Group { get; private set; }
+    }
+}
