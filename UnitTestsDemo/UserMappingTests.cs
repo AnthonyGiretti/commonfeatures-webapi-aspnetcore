f899a7a0596c80fde22dcaeeef49f32d52ed7a8e
Anthony Giretti <anthony.giretti@gmail.com>
2018-12-16 22:12:41 -0500

    Unit test automapper sample + adding ExpectedObject

---
+++ UnitTestsDemo/UserMappingTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using FluentValidation.Results;
+using WebApiDemo.Validators;
+using Xunit;
+using System.Linq;
+using WebApiDemo.Models;
+using WebApiDemo.Enum;
+using AutoMapper.Configuration;
+using AutoMapper;
+using WebApiDemo.Mapping;
+using ExpectedObjects;
+
+namespace UnitTestsDemo
+{
+    public class UserMappingTests
+    {
+        public UserMappingTests()
+        {
+            var mappings = new MapperConfigurationExpression();
+            mappings.AddProfile<MyMappingProfiles>();
+            Mapper.Initialize(mappings);
+        }
+
+        [Fact]
+        public void WhenUserEntityIsWellSet_UserIsWellMapped()
+        {
+            // Arrange
+            var entity = new UserEntity
+            {
+                Gender = Gender.Mister,
+                FirstName = "Anthony",
+                LastName = "Giretti",
+                Id = "123456789"
+            };
+
+            var expected = new User
+            {
+                Gender = "Mister",
+                FirstName = "ANTHONY",
+                LastName = "GIRETTI",
+                SIN = "123456789"
+            }.ToExpectedObject();
+
+            // Act
+            var user = Mapper.Map<User>(entity);
+
+            // Assert
+            expected.ShouldEqual(user);
+        }
+    }
+}
