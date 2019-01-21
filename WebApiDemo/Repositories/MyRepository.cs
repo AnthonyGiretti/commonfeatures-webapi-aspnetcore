4f2d0edc6648877fff61b0129d4e833ba7c0e0fe
Anthony Giretti <anthony.giretti@gmail.com>
2019-01-20 19:40:27 -0500

    delete useless using

--- WebApiDemo/Repositories/MyRepository.cs
+++ WebApiDemo/Repositories/MyRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebApiDemo.Models;
+﻿using WebApiDemo.Models;
 using WebApiDemo.Services;
 
 namespace WebApiDemo.Repositories
@@ -18,6 +14,8 @@ namespace WebApiDemo.Repositories
 
         public UserEntity GetUserById(int id)
         {
+            // code to get data here.....
+
             return new UserEntity();
         }
     }
