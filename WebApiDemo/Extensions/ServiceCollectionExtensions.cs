725469568bfceeba3667f75b5a2cdb985e1652e2
Anthony Giretti <anthony.giretti@gmail.com>
2019-01-23 20:07:15 -0500

    refactor AddScopedDynamic method

---
+++ WebApiDemo/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddScopedDynamic<TInterface>(this IServiceCollection services, List<Type> types)
+        {
+            services.AddScoped<Func<string, TInterface>>(serviceProvider => tenant =>
+            {
+                var type = types.Where(y => y.GetInterfaces().Contains(typeof(TInterface)))
+                                          .Where(x => x.Name.Contains(tenant))
+                                          .FirstOrDefault();
+                if (null == type)
+                    throw new KeyNotFoundException("Aucune instance trouvée pour le tenant fournit.");
+
+                return (TInterface)serviceProvider.GetService(type);
+            });
+        }
+    }
+}
