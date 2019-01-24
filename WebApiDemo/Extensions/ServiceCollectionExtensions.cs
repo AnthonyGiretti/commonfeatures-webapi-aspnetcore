5efb3050a409db20a1292a4a2d7d43627b10df24
Anthony Giretti <anthony.giretti@gmail.com>
2019-01-23 20:16:26 -0500

    Refactor AddScopedDynamic

--- WebApiDemo/Extensions/ServiceCollectionExtensions.cs
+++ WebApiDemo/Extensions/ServiceCollectionExtensions.cs
@@ -2,19 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApiDemo.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddScopedDynamic<TInterface>(this IServiceCollection services, List<Type> types)
+        public static void AddScopedDynamic<TInterface>(this IServiceCollection services, IEnumerable<Type> types)
         {
             services.AddScoped<Func<string, TInterface>>(serviceProvider => tenant =>
             {
-                var type = types.Where(y => y.GetInterfaces().Contains(typeof(TInterface)))
-                                          .Where(x => x.Name.Contains(tenant))
-                                          .FirstOrDefault();
+                var type = types.FilterByInterface<TInterface>()
+                                .Where(x => x.Name.Contains(tenant))
+                                .FirstOrDefault();
+
                 if (null == type)
                     throw new KeyNotFoundException("Aucune instance trouvée pour le tenant fournit.");
 
---
+++ WebApiDemo/Extensions/TypesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Extensions
+{
+    public static class TypesExtensions
+    {
+        public static IEnumerable<Type> FilterByInterface<TInterface>(this IEnumerable<Type> types)
+        {
+            return types.Where(y => y.GetInterfaces().Contains(typeof(TInterface)));
+        }
+    }
+}
