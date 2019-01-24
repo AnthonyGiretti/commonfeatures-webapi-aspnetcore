016b7c5db8d1255468fdff1ad3c88bd1a8663025
Anthony Giretti <anthony.giretti@gmail.com>
2019-01-23 21:24:52 -0500

    add guid to check instance lifetime

--- WebApiDemo/Services/Tenants/Tenant1/Tenant1Service.cs
+++ WebApiDemo/Services/Tenants/Tenant1/Tenant1Service.cs
@@ -1,19 +1,27 @@
-﻿using WebApiDemo.Repositories;
+﻿using System;
+using WebApiDemo.Repositories;
 
 namespace WebApiDemo.Services.Tenants.Tenant1
 {
     public class Tenant1Service : ITenantService
     {
         private IMyRepository _myRepository;
+        private Guid _guid;
 
         public Tenant1Service(IMyRepository myRepository)
         {
             _myRepository = myRepository;
+            _guid = Guid.NewGuid();
         }
 
         public string GetName()
         {
-            return "Tenant1";
+            return "Tenant1, instance id : " + GetGuid();
+        }
+
+        public string GetGuid()
+        {
+            return _guid.ToString();
         }
     }
 }
--- WebApiDemo/Services/Tenants/Tenant2/Tenant2Service.cs
+++ WebApiDemo/Services/Tenants/Tenant2/Tenant2Service.cs
@@ -9,15 +9,22 @@ namespace WebApiDemo.Services.Tenants.Tenant2
     public class Tenant2Service : ITenantService
     {
         private IMyRepository _myRepository;
+        private Guid _guid;
 
         public Tenant2Service(IMyRepository myRepository)
         {
             _myRepository = myRepository;
+            _guid = Guid.NewGuid();
         }
 
         public string GetName()
         {
-            return "Tenant2";
+            return "Tenant2, instance id : " + GetGuid();
+        }
+
+        private string GetGuid()
+        {
+            return _guid.ToString();
         }
     }
 }
