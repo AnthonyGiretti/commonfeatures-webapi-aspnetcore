e3ed301a2ed1959e2ab3cca3646b3db3c35e6244
Anthony Giretti <anthony.giretti@gmail.com>
2019-02-03 15:32:49 -0500

    improvements

--- UnitTestsDemo/CustomExceptionMiddlewareTests.cs
+++ UnitTestsDemo/CustomExceptionMiddlewareTests.cs
@@ -21,10 +21,11 @@ namespace UnitTestsDemo
         {
             // Arrange
             var loggerMock = Substitute.For<ILogger<CustomExceptionMiddleware>>();
-            var middleware = new CustomExceptionMiddleware((innerHttpContext) =>
+            RequestDelegate requestDelegate = (innerHttpContext) =>
             {
                 throw new Exception("Oooops error!");
-            }, loggerMock);
+            };
+            var middleware = new CustomExceptionMiddleware(requestDelegate, loggerMock);
 
             // Use DefaultHttpContext insteadof mocking HttpContext
             var context = new DefaultHttpContext();
@@ -35,6 +36,7 @@ namespace UnitTestsDemo
             //Act
             await middleware.Invoke(context);
 
+            //Assert
             // set the position to beginning before reading
             context.Response.Body.Seek(0, SeekOrigin.Begin);
 
@@ -43,7 +45,7 @@ namespace UnitTestsDemo
             var streamText = reader.ReadToEnd();
             var objResponse = JsonConvert.DeserializeObject<Error>(streamText);
 
-            //Assert
+            
             objResponse
             .Should()
             .BeEquivalentTo(new { Message = "Unhandled error", Errors = new List<string>() , Code = "00009" });
@@ -51,6 +53,10 @@ namespace UnitTestsDemo
             context.Response.StatusCode
             .Should()
             .Be((int)HttpStatusCode.InternalServerError);
+
+            context.Response.ContentType
+            .Should()
+            .Be("application/json");
 
             loggerMock.Received(1);
         }
