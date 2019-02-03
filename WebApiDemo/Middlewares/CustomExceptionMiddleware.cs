8a4cb4bf29dd6bbd8e142d10348ff9bf198e5b80
Anthony Giretti <anthony.giretti@gmail.com>
2019-02-03 11:33:37 -0500

    remove useless using

--- WebApiDemo/Middlewares/CustomExceptionMiddleware.cs
+++ WebApiDemo/Middlewares/CustomExceptionMiddleware.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using WebApiDemo.Models;
