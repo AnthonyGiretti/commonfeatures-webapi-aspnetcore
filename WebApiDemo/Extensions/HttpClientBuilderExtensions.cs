66202dd2eae0a55ce211644d526902148bb7dd80
Anthony Giretti <anthony.giretti@gmail.com>
2019-02-14 20:03:11 -0500

    Add HttpClientBuilderExtensions

---
+++ WebApiDemo/Extensions/HttpClientBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebApiDemo.RetryPolicies;
+using WebApiDemo.RetryPolicies.Config;
+
+namespace WebApiDemo.Extensions
+{
+    public static class HttpClientBuilderExtensions
+    {
+        public static IHttpClientBuilder AddPolicyHandlers(this IHttpClientBuilder httpClientBuilder, string policySectionName, ILoggerFactory loggerFactory, IConfiguration configuration)
+        {
+            var retryLogger = loggerFactory.CreateLogger("PollyHttpRetryPoliciesLogger");
+            var circuitBreakerLogger = loggerFactory.CreateLogger("PollyHttpCircuitBreakerPoliciesLogger");
+
+            var policyConfig = new PolicyConfig();
+            configuration.Bind("PolicyConfig", policyConfig);
+
+            var circuitBreakerPolicyConfig = (ICircuitBreakerPolicyConfig)policyConfig;
+            var retryPolicyConfig = (IRetryPolicyConfig)policyConfig;
+
+            return httpClientBuilder.AddRetryPolicyHandler(retryLogger, retryPolicyConfig)
+                                    .AddCircuitBreakerHandler(circuitBreakerLogger, circuitBreakerPolicyConfig);
+        }
+
+        public static IHttpClientBuilder AddRetryPolicyHandler(this IHttpClientBuilder httpClientBuilder, ILogger logger, IRetryPolicyConfig retryPolicyConfig)
+        {
+            return httpClientBuilder.AddPolicyHandler(HttpRetryPolicies.GetHttpRetryPolicy(logger, retryPolicyConfig));
+        }
+
+        public static IHttpClientBuilder AddCircuitBreakerHandler(this IHttpClientBuilder httpClientBuilder, ILogger logger, ICircuitBreakerPolicyConfig circuitBreakerPolicyConfig)
+        {
+            return httpClientBuilder.AddPolicyHandler(HttpCircuitBreakerPolicies.GetHttpCircuitBreakerPolicy(logger, circuitBreakerPolicyConfig));
+        }
+    }
+}
---
+++ WebApiDemo/RetryPolicies/HttpRetryPolicies.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+using WebApiDemo.RetryPolicies.Config;
+
+namespace WebApiDemo.RetryPolicies
+{
+    public static class HttpRetryPolicies
+    {
+        public static RetryPolicy<HttpResponseMessage> GetHttpRetryPolicy(ILogger logger, IRetryPolicyConfig retryPolicyConfig)
+        {
+            return HttpPolicyBuilders.GetDefaultBuilder()
+                                          .WaitAndRetryAsync(retryPolicyConfig.RetryCount,
+                                                             ComputeDuration,
+                                                             (result, timeSpan, retryCount, context) =>
+                                                             {
+                                                                 OnHttpRetry(result, timeSpan, retryCount, context, logger);
+                                                             });
+        }
+
+        private static void OnHttpRetry(DelegateResult<HttpResponseMessage> result, TimeSpan timeSpan, int retryCount, Polly.Context context, ILogger logger)
+        {
+            if (result.Result != null)
+            {
+                logger.LogWarning("Request failed with {StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}", result.Result.StatusCode, timeSpan, retryCount);
+            }
+            else
+            {
+                logger.LogWarning("Request failed because network failure. Waiting {timeSpan} before next retry. Retry attempt {retryCount}", timeSpan, retryCount);
+            }
+        }
+
+        private static TimeSpan ComputeDuration(int input)
+        {
+            return TimeSpan.FromSeconds(Math.Pow(2, input)) + TimeSpan.FromMilliseconds(new Random().Next(0, 100));
+        }
+    }
+}
