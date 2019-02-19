a15873c078ffbf2ae42ca45afe01cafb5d23b8ba
Anthony Giretti <anthony.giretti@gmail.com>
2019-02-18 20:04:28 -0500

    create EF CRUD method in repository

--- WebApiDemo/Repositories/EFCountryRepository.cs
+++ WebApiDemo/Repositories/EFCountryRepository.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using WebApiDemo.Database;
+using WebApiDemo.Models;
+using Z.EntityFramework.Plus;
+using System.Linq;
 
 namespace WebApiDemo.Repositories
 {
@@ -12,6 +14,52 @@ namespace WebApiDemo.Repositories
         public EFCountryRepository(DemoDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public async Task<List<Country>> Get()
+        {
+            return await _dbContext.Country.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Country> GetById(int countryId)
+        {
+            return await _dbContext.Country.AsNoTracking().FirstOrDefaultAsync(x => x.CountryId == countryId);
+        }
+
+        public async Task<long> Add(Country country)
+        {
+            _dbContext.Add(country);
+            await _dbContext.SaveChangesAsync();
+            return country.CountryId;
+        }
+
+        public async Task<int> Update(Country country)
+        {
+            return await _dbContext.Country
+                                    .Where(x => x.CountryId == country.CountryId)
+                                    .UpdateAsync(x => new Country
+                                    {
+                                        CountryName = country.CountryName,
+                                        Description = country.Description
+                                    });
+        }
+
+        public async Task<int> UpdateDescription(int countryId, string description)
+        {
+            return await _dbContext.Country
+                                    .Where(x => x.CountryId == countryId)
+                                    .UpdateAsync(x => new Country
+                                    {
+                                        Description = description
+                                    });
+
+        }
+
+        public async Task<int> Delete(int countryId)
+        {
+            return await _dbContext.Country
+                                    .Where(x => x.CountryId == countryId)
+                                    .DeleteAsync();
         }
     }
 }
