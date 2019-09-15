24480c1f1a370d40939119c3376c8ad469144f3b
Anthony Giretti <anthony.giretti@gmail.com>
2019-09-14 20:15:50 -0400

    add smtp configuration

---
+++ WebApiDemo/Models/SmtpConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Models
+{
+    public class SmtpConfiguration
+    {
+        public string Domain { get; set; }
+        public int Port { get; set; }
+    }
+}
