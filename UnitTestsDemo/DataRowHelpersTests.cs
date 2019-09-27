49ae1eb044e361d07d8b8e61759b4920c9d3494b
Anthony Giretti <anthony.giretti@gmail.com>
2019-09-26 22:52:05 -0400

    add new tests

--- UnitTestsDemo/DataRowHelpersTests.cs
+++ UnitTestsDemo/DataRowHelpersTests.cs
@@ -1,6 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoFixture;
+using ExpectedObjects;
+using Newtonsoft.Json;
+using System.Data;
+using System.Linq;
+using WebApiDemo.Helpers;
+using WebApiDemo.Models;
+using Xunit;
+using static UnitTestsDemo.TransactionServiceTests;
 
 namespace UnitTestsDemo
 {
@@ -8,14 +14,30 @@ namespace UnitTestsDemo
     {
         public class ToTransactionTests
         {
+            private Fixture _fixture;
+            public ToTransactionTests()
+            {
+                _fixture = new Fixture();
+            }
+
+            [Fact]
             public void WhenDataRowIsNotNull_ToTransactionShouldMapCorrectlyIntoTransactionObject()
             {
                 // Arrange
-            }
+                var transactionData = _fixture.CreateMany<TransactionData>(1).ToList();
+                var dataTable = JsonConvert.DeserializeObject<DataTable>(JsonConvert.SerializeObject(transactionData));
+                var expectedResult = new Transaction
+                {
+                    TransactionId = transactionData[0].Id,
+                    TransactionDate = transactionData[0].Date,
+                    TransactionAmount = transactionData[0].Amount,
+                }.ToExpectedObject();
 
-            public void WhenDataRowIsNull_ToTransactionShouldReturnNull()
-            {
-                // Arrange
+                // Act
+                var transaction = dataTable.Rows[0].ToTransaction();
+
+                // Assert
+                expectedResult.ShouldEqual(transaction);
             }
         }
     }
